67733bec681a2d79fe1080a4702590650e371ba1
anzarmalik <anzarmalikcse@gmail.com>
2021-07-18 14:17:22 +0530

    added schema

--- schema.js
+++ schema.js
@@ -1,8 +1,34 @@
-import {} from 'graphql';
+import {buildSchema} from 'graphql';
 
 const schema = buildSchema(`
+    type Course {
+        id : ID 
+        courseName : String
+        category : String
+        price : Int
+        language : String
+        email : String
+        stack : Stack
+        teachingAssists : [TeachAssist]
+    }
 
-`);
+    type TeachAssist {
+        firstName : String
+        lastName : String
+        experience : Int 
+    }
+
+    enum Stack {
+        WEB
+        MOBILE
+        OTHER
+    }
+
+
+    type Query {
+        getCourse(id: ID):Course
+    }
+`); 
 
 
 
